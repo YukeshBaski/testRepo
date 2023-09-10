221d59cd8f4a8f7fb0a1ed0da68a15dc889d25cc
YukeshBaski <yukeshbaski2@gmail.com>
2023-09-10 08:17:10 +0530

    added a generic function

--- main.go
+++ main.go
@@ -4,4 +4,10 @@ import "fmt"
 
 func main() {
 	fmt.Println("first staging area.")
+	genericFunc("hello")
+	genericFunc(123)
+}
+
+func genericFunc[k string | int](thing k) {
+	fmt.Println(thing)
 }
